6032897021c5563e9c66abc98cfc9d64492e2e5c
abdulrahmanahme <abdelramanahmed00@gmail.com>
2023-08-04 00:20:26 +0200

    Add model of articles data, create get function data getArticle , add app constances and passing data to the UI

---
+++ lib/presentation/screens/article_screen.dart
@@ -0,0 +1,93 @@
+import 'package:artical/core/widget/widget.dart';
+import 'package:flutter/material.dart';
+
+class ArticlesScreen extends StatefulWidget {
+  const ArticlesScreen(
+      {super.key,
+      this.title,
+      this.abstractBrief,
+      this.byline,
+      this.article,
+      this.date,
+      this.source,
+      this.image});
+  final String? title;
+  final String? abstractBrief;
+  final String? byline;
+  final String? date;
+  final String? source;
+  final String? article;
+  final String? image;
+
+  @override
+  State<ArticlesScreen> createState() => _ArticlesScreenState();
+}
+
+class _ArticlesScreenState extends State<ArticlesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(
+          Icons.arrow_back,
+          size: 30,
+          color: Colors.white,
+        ),
+        title: const TextWidget(
+          name: 'NY Times Most Popular',
+          fontSize: 20,
+          color: Colors.white,
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xff45E3C2),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TextWidget(
+              padding: 5.0,
+              name: widget.title,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            TextWidget(
+              padding: 5.0,
+              name: widget.abstractBrief,
+              color: Colors.grey,
+              fontSize: 15,
+              fontWeight: FontWeight.normal,
+            ),
+            Padding(
+              padding:const EdgeInsets.only(left: 8, right: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextWidget(
+                    name: widget.byline,
+                  ),
+                  TextWidget(
+                    name: widget.date,
+                  ),
+                ],
+              ),
+            ),
+            ImageWidget(
+              image: widget.image,
+            ),
+            TextWidget(
+              name: widget.source,
+            ),
+            TextWidget(
+              padding: 5.0,
+              name: widget.article,
+              color: Colors.black,
+              fontSize: 15,
+              fontWeight: FontWeight.normal,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,5 +1,8 @@
-import 'package:artical/core/widget.dart';
+import 'package:artical/core/widget/widget.dart';
+import 'package:artical/presentation/screens/article_screen.dart';
 import 'package:flutter/material.dart';
+import '../../data/datasource/remote_data/articels_reomte_data_source.dart';
+import '../../domain/entites/articels_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,8 +12,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  ArticlesRemoteDataSource? dataSource;
+  List<Results>? data;
+  bool isLoading = true;
+  @override
+  void initState() {
+    ArticlesRemoteDataSource().getArticels().then((value) {
+      setState(() {
+        isLoading = false;
+        data = value;
+        print('ddsssss${data![1].media![0].mediaMetadata![1].url}');
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
         leading: const IconWidget(
@@ -27,9 +46,42 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: const Color(0xff45E3C2),
         actions: appBarWidget,
       ),
-      body: const Column(
+      body: Column(
         children: [
-          ArtivalWidget(),
+          isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ArticlesScreen(
+                              title: data![index].title.toString(),
+                              abstractBrief:
+                                  data![index].abstractBrief.toString(),
+                              byline: data![index].byline.toString(),
+                              date: data![index].publishedDate.toString(),
+                              image:
+                                  data![index].media![0].mediaMetadata![2].url,
+                              source: data![index].source.toString(),
+                              article: data![index].adxKeywords.toString(),
+                            ),
+                          ),
+                        );
+                      },
+                      child: ArticleWidget(
+                        title: data![index].title.toString(),
+                        byline: data![index].byline.toString(),
+                        date: data![index].publishedDate.toString(),
+                        image: data![index].media![0].mediaMetadata![1].url,
+                      ),
+                    );
+                  },
+                )
         ],
       ),
     );
