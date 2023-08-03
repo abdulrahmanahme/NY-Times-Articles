c640133351fd2230c4f83843b8428a101b2d4821
abdulrahmanahme <abdelramanahmed00@gmail.com>
2023-08-04 00:17:17 +0200

    undo commit

--- lib/core/app_constances/app_constances.dart
+++
@@ -1,4 +0,0 @@
-class AppConstances{
- static const baseUrl='https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=$appKey';
- static const appKey='nRIxlGMeFJbqmEdRXk8Prvtr41GUERYo';
- }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,8 +1,5 @@
-import 'package:artical/core/widget/widget.dart';
-import 'package:artical/presentation/screens/article_screen.dart';
+import 'package:artical/core/widget.dart';
 import 'package:flutter/material.dart';
-import '../../data/datasource/remote_data/articels_reomte_data_source.dart';
-import '../../domain/entites/articels_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,24 +9,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  ArticlesRemoteDataSource? dataSource;
-  List<Results>? data;
-  bool isLoading = true;
-  @override
-  void initState() {
-    ArticlesRemoteDataSource().getArticels().then((value) {
-      setState(() {
-        isLoading = false;
-        data = value;
-        print('ddsssss${data![1].media![0].mediaMetadata![1].url}');
-      });
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         leading: const IconWidget(
@@ -46,42 +27,9 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: const Color(0xff45E3C2),
         actions: appBarWidget,
       ),
-      body: Column(
+      body: const Column(
         children: [
-          isLoading
-              ? const Center(child: CircularProgressIndicator())
-              : ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ArticlesScreen(
-                              title: data![index].title.toString(),
-                              abstractBrief:
-                                  data![index].abstractBrief.toString(),
-                              byline: data![index].byline.toString(),
-                              date: data![index].publishedDate.toString(),
-                              image:
-                                  data![index].media![0].mediaMetadata![2].url,
-                              source: data![index].source.toString(),
-                              article: data![index].adxKeywords.toString(),
-                            ),
-                          ),
-                        );
-                      },
-                      child: ArticleWidget(
-                        title: data![index].title.toString(),
-                        byline: data![index].byline.toString(),
-                        date: data![index].publishedDate.toString(),
-                        image: data![index].media![0].mediaMetadata![1].url,
-                      ),
-                    );
-                  },
-                )
+          ArtivalWidget(),
         ],
       ),
     );
